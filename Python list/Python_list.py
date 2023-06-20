02deb227923289e5ed681c521ab8225ea0bedafa
st1ko-gs <antonluomala2008@gmail.com>
2023-06-20 05:58:50 +0300

    added extend and code+ documentation example

--- Python list/Python_list.py
+++ Python list/Python_list.py
@@ -6,7 +6,10 @@ camping_list = ["knife", "tent", "food", "water", "sleeping bags", "coffee"]
 
 camp_site = ["Lake", 404, 28.3, True]
 
+#instead of adding it to the list we use "append" 
 camping_list.append("phone")
+camping_list.append("charger")
+camping_list.extend(["tobak, snus"])
 
 print (camping_list)
 
