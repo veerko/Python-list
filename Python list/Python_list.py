fa9f1e7a29a67ebcec6a6e1705cb462c3fbb7170
st1ko-gs <antonluomala2008@gmail.com>
2023-06-20 06:59:19 +0300

    added some more things with "append"

--- Python list/Python_list.py
+++ Python list/Python_list.py
@@ -9,6 +9,10 @@ camp_site = ["Lake", 404, 28.3, True]
 #instead of adding it to the list we use "append" 
 camping_list.append("phone")
 camping_list.append("charger")
+camping_list.append("flash drive")
+camping_list.append("ethernet")
+camping_list.append("rasperry pi")
+#instead of append we can also use extend, where on append we can only add 1 item in a row, but with extend we can add more in the same row
 camping_list.extend(["tobak, snus"])
 
 print (camping_list)
