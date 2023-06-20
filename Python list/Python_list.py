fc66b4c0db1ac68ef708d7426eab1f143dcd468b
st1ko-gs <antonluomala2008@gmail.com>
2023-06-20 05:52:19 +0300

    Added "append"+ code example

--- Python list/Python_list.py
+++ Python list/Python_list.py
@@ -1,20 +1,13 @@
 
-#just basic python str=strings
-camping_stuff = "knife", "tent", "food", "water", "sleeping bags", "coffee"
-
-print (camping_stuff)
-
 
 #PYTHON LIST
+
 camping_list = ["knife", "tent", "food", "water", "sleeping bags", "coffee"]
 
 camp_site = ["Lake", 404, 28.3, True]
 
-me = camping_list[0]
+camping_list.append("phone")
 
-you = camping_list[-1]
+print (camping_list)
 
-print = (me)
-print = (you)
 
-print = camping_list
