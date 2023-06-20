6f212130deeb6be55ad202fd3331790ac38c2401
st1ko-gs <antonluomala2008@gmail.com>
2023-06-20 05:44:47 +0300

    added camp_site

--- Python list/Python_list.py
+++ Python list/Python_list.py
@@ -1,9 +1,20 @@
 
-
-camping_stuff = "knife", "tent", "food", "water"
+#just basic python str=strings
+camping_stuff = "knife", "tent", "food", "water", "sleeping bags", "coffee"
 
 print (camping_stuff)
 
-camping_list = ["knife", "tent", "food", "water"]
 
-print(type(camping_list))
+#PYTHON LIST
+camping_list = ["knife", "tent", "food", "water", "sleeping bags", "coffee"]
+
+camp_site = ["Lake", 404, 28.3, True]
+
+me = camping_list[0]
+
+you = camping_list[-1]
+
+print = (me)
+print = (you)
+
+print = camping_list
